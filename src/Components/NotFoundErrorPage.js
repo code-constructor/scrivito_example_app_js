9bfa789f719d2ddfce5932cc5b31bcb86cab16d4
Alexander Schrot <alexander.schrot@infopark.de>
2018-02-26 11:30:05 +0100

    Redirect to /404 path if the NotFoundErrorPage did mount

--- src/Components/NotFoundErrorPage.js
+++ src/Components/NotFoundErrorPage.js
@@ -2,29 +2,39 @@ import * as React from 'react';
 import * as Scrivito from 'scrivito';
 import Helmet from 'react-helmet';
 
-function NotFoundErrorPage() {
-  const backgroundImage = [
-    'linear-gradient(rgba(46, 53, 60, 0.7), rgba(46, 53, 60, 0.7))',
-    'url(https://unsplash.com/photos/Bs0zgYkYEZw/download)',
-  ].join(', ');
+class NotFoundErrorPage extends React.Component {
+  componentDidMount() {
+    const needsRedirect = window.location.pathname !== '/404';
 
-  return <Scrivito.NotFoundErrorPage>
-    <section
-      className="bg-dark-image full-height"
-      style={ { background: 'no-repeat center / cover', backgroundImage } }
-    >
-      <div className="container">
-        <div className="text-center"><h1 className="hero-bold">Ooops</h1></div>
-        <div className="text-center">
-          <h2 className="hero-small">The page you are looking for does not exist.</h2>
+    if (needsRedirect) {
+      window.location.replace(`/404`);
+    }
+  }
+
+  render() {
+    const backgroundImage = [
+      'linear-gradient(rgba(46, 53, 60, 0.7), rgba(46, 53, 60, 0.7))',
+      'url(https://unsplash.com/photos/Bs0zgYkYEZw/download)',
+    ].join(', ');
+
+    return <Scrivito.NotFoundErrorPage>
+      <section
+        className="bg-dark-image full-height"
+        style={ { background: 'no-repeat center / cover', backgroundImage } }
+      >
+        <div className="container">
+          <div className="text-center"><h1 className="hero-bold">Ooops</h1></div>
+          <div className="text-center">
+            <h2 className="hero-small">The page you are looking for does not exist.</h2>
+          </div>
+          <div className="text-center">
+            <LinkToRoot />
+          </div>
         </div>
-        <div className="text-center">
-          <LinkToRoot />
-        </div>
-      </div>
-    </section>
-    <Helmet meta={ [{ name: 'prerender-status-code', content: '404' }] } />
-  </Scrivito.NotFoundErrorPage>;
+      </section>
+      <Helmet meta={ [{ name: 'prerender-status-code', content: '404' }] } />
+    </Scrivito.NotFoundErrorPage>;
+  }
 }
 
 function PlainLinkToRoot() {
